9df21ba5509776e97160f9a42e748f373a004a9d
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-05 09:15:44 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-const App = () => {
-  return <p>I am learning React. My life is getting better.</p>;
-};
+class App extends Component {
+  render() {
+    return <p>I am learning React. My life is getting better. </p>;
+  }
+}
 
 export default App;
