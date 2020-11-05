f3ae6be970df75153bae7187e2f58d90b43f3d93
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-05 09:54:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React, { Component, useState } from "react";
-import "../styles/App.css";
+import React, {Component, useState} from "react";
+import '../styles/App.css';
 
-class App extends Component {
-  render() {
-    return <p>I am learning React. My life is getting better. </p>;
-  }
+const App = () => {
+  return (
+    <p>I am learning React. My life is getting better. </p>
+  )
 }
+
 
 export default App;
